0f88dd75f079a25944bf56722bfda0ea385ceb78
Jeffruiz1502003 <123280099+Jeffruiz1502003@users.noreply.github.com>
2023-09-26 16:20:52 -0400

    adding changes

--- src/scripts/scrollReveal.js
+++ src/scripts/scrollReveal.js
@@ -1,9 +1,9 @@
-// export default function initScrollReveal(targetElements, defaultProps) {
-//   if (!targetElements.length) return;
+export default function initScrollReveal(targetElements, defaultProps) {
+  if (!targetElements.length) return;
 
-//   ScrollReveal({ reset: false });
+  ScrollReveal({ reset: false });
 
-//   targetElements.forEach(({ element, animation }) => {
-//     ScrollReveal().reveal(element, Object.assign({}, defaultProps, animation));
-//   });
-// }
+  targetElements.forEach(({ element, animation }) => {
+    ScrollReveal().reveal(element, Object.assign({}, defaultProps, animation));
+  });
+}
